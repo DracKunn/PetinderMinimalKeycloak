721bdacd789d05bfb885cc3adb8876aa0c2802ff
DracKunn <brecht.vanrie@gmail.com>
2022-06-03 14:38:22 +0200

    petinder light for keycloack

--- src/main/java/com/switchfully/petinderminimal/api/PetController.java
+++ src/main/java/com/switchfully/petinderminimal/api/PetController.java
@@ -2,6 +2,7 @@ package com.switchfully.petinderminimal.api;
 
 import com.switchfully.petinderminimal.model.PetDTO;
 import com.switchfully.petinderminimal.service.PetService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class PetController {
     return "Hello world!";
   }
 
+  @PreAuthorize("hasAuthority('VIEW_PETS')")
   @GetMapping(produces = "application/json")
   public List<PetDTO> getAllPets() {
     return petService.getAllPets();
