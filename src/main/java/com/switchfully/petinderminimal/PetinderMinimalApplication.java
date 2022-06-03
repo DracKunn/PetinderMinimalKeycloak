f71700c5ca36cb77e08e402dacf072aa2def4469
DracKunn <brecht.vanrie@gmail.com>
2022-06-03 15:27:32 +0200

    petinder light for keycloack

--- src/main/java/com/switchfully/petinderminimal/PetinderMinimalApplication.java
+++ src/main/java/com/switchfully/petinderminimal/PetinderMinimalApplication.java
@@ -2,8 +2,12 @@ package com.switchfully.petinderminimal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"controller", "services", "configure"}) // This is what you need
+@EntityScan("com.switchfully.petinderminimal")
 public class PetinderMinimalApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/switchfully/petinderminimal/security/WebSecurityConfig.java
+++ src/main/java/com/switchfully/petinderminimal/security/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.switchfully.petinderminimal.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -15,12 +13,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
+                .disable().authorizeRequests().antMatchers("/users").permitAll()
                 .and().authorizeRequests().anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
 
     }
 
-    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
         converter.setJwtGrantedAuthoritiesConverter(new KeycloakGrantedAuthoritiesConverter());
         return converter;
